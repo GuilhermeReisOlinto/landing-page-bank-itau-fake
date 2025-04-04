c940a3304c5c6bc84c91615746d57c94c8f43f64
guilherme olinto <ctl_golinto@uolinc.com>
2025-04-04 15:49:21 -0300

    feat:create login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,18 @@
 export default function LoginPage() {
     return (
-        <form className="login-form">
-            <div className="flex items-center gap-3 display flex-col">
-                <h1 className="text-3xl font-bold">Login</h1>
-                <label><b>Username: </b></label>
-                <input type="text" placeholder="Enter your username" name="username" required />
+        <div className="flex items-center justify-center h-screen">
+            <form className="flex flex-col m-5 p-4 border border-gray-400 bg-white w-[32rem] h-[32rem] shadow-lg rounded-lg">
+                <div className="flex p-8 pl-6 gap-3 display flex-col">
+                    <h1 className="text-3xl p-5 font-bold">Login</h1>
+                    <label><b>Username: </b></label>
+                    <input className="flex p-5" type="text" placeholder="Enter your username" name="username" required />
 
-                <label><b>Password: </b></label>
-                <input type="password" placeholder="Enter your password" name="pass" required />
+                    <label><b>Password: </b></label>
+                    <input className="flex p-5" type="password" placeholder="Enter your password" name="pass" required />
 
-                <button type="submit">Login</button>
-            </div>
-        </form >
+                    <button className="mt-10 px-6 py-3 bg-orange-400 text-white rounded-lg" type="submit">Login</button>
+                </div>
+            </form >
+        </div>
     )
 }
