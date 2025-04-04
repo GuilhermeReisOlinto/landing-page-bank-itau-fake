2d65561e98fb1daa993a7b762db174ea65bd5b7a
guilherme olinto <ctl_golinto@uolinc.com>
2025-04-04 10:02:23 -0300

    feat: creating login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,16 @@
 export default function LoginPage() {
     return (
-        <div>
-            <label><b>Username</b></label>
-        </div>
+        <form className="login-form">
+            <div className="flex items-center gap-3 display flex-col">
+                <h1 className="text-3xl font-bold">Login</h1>
+                <label><b>Username: </b></label>
+                <input type="text" placeholder="Enter your username" name="username" required />
+
+                <label><b>Password: </b></label>
+                <input type="password" placeholder="Enter your password" name="pass" required />
+
+                <button type="submit">Login</button>
+            </div>
+        </form >
     )
 }
